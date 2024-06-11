275509fb559690d3257831126851d33e00755f41
Mendes, Eric Silva <ericsilvaccp@gmail.com>
2024-06-11 12:36:53 -0300

    refactor: create CardServiceStub

---
+++ src/test/java/com/eric218110/project/zeta/data/usecases/card/CardServiceStub.java
@@ -0,0 +1,49 @@
+package com.eric218110.project.zeta.data.usecases.card;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+import com.eric218110.project.zeta.data.entities.card.CardEntity;
+import com.eric218110.project.zeta.data.entities.user.UserEntity;
+import com.eric218110.project.zeta.domain.enums.cardtype.CardTypesEnum;
+import com.eric218110.project.zeta.domain.http.card.AddCardRequest;
+import com.github.javafaker.Faker;
+
+public class CardServiceStub {
+  private final static Faker faker = new Faker();
+
+  static AddCardRequest loadAddCardDto() {
+    return AddCardRequest.builder().name(CardServiceStub.faker.name().fullName())
+        .color(CardServiceStub.faker.color().hex())
+        .currentLimit(CardServiceStub.faker.number().randomDouble(10, 0, 100))
+        .flag(CardServiceStub.faker.random().toString())
+        .dueDate(CardServiceStub.faker.date().toString())
+        .closeDate(CardServiceStub.faker.date().toString()).type(CardTypesEnum.CREDIT.toString())
+        .build();
+  }
+
+  static List<CardEntity> loadFakeCardEntities() {
+
+    CardEntity cardEntityOne = CardServiceStub.loadFakeCardEntity();
+    CardEntity cardEntityTwo = CardServiceStub.loadFakeCardEntity();
+
+    return List.of(cardEntityOne, cardEntityTwo);
+  }
+
+  static CardEntity loadFakeCardEntity() {
+
+    return CardEntity.builder().uuid(CardServiceStub.faker.idNumber().toString())
+        .name(CardServiceStub.faker.name().fullName()).color(CardServiceStub.faker.color().hex())
+        .currentLimit(CardServiceStub.faker.number().randomDouble(10, 0, 100))
+        .flag(CardServiceStub.faker.random().toString())
+        .dueDate(CardServiceStub.faker.date().toString())
+        .closeDate(CardServiceStub.faker.date().toString()).type(CardTypesEnum.CREDIT.toString())
+        .build();
+  }
+
+  static Optional<UserEntity> loadUserEntity() {
+    return Optional.of(UserEntity.builder()
+        .username(CardServiceStub.faker.internet().emailAddress())
+        .password(CardServiceStub.faker.internet().password()).userId(UUID.randomUUID()).build());
+  }
+}
--- src/test/java/com/eric218110/project/zeta/data/usecases/card/CardServiceTests.java
+++ src/test/java/com/eric218110/project/zeta/data/usecases/card/CardServiceTests.java
@@ -4,7 +4,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -12,9 +11,6 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import com.eric218110.project.zeta.data.entities.card.CardEntity;
-import com.eric218110.project.zeta.data.entities.user.UserEntity;
-import com.eric218110.project.zeta.domain.enums.cardtype.CardTypesEnum;
-import com.eric218110.project.zeta.domain.http.card.AddCardRequest;
 import com.eric218110.project.zeta.domain.http.card.ShowCardResponse;
 import com.eric218110.project.zeta.infra.repositories.database.card.CardRepository;
 import com.eric218110.project.zeta.infra.repositories.database.user.UserRepository;
@@ -40,9 +36,9 @@ class CardServiceTests {
 
   @Test
   void listAllReturnsValuesCorrect() {
-    List<CardEntity> cardEntities = loadFakeCardEntities();
+    List<CardEntity> cardEntities = CardServiceStub.loadFakeCardEntities();
     when(this.cardRepository.findByUser(any())).thenReturn(cardEntities);
-    when(this.userRepository.findById(any())).thenReturn(loadUserEntity());
+    when(this.userRepository.findById(any())).thenReturn(CardServiceStub.loadUserEntity());
 
     List<ShowCardResponse> result = cardService.listAllByUserUuid(UUID.randomUUID());
 
@@ -52,7 +48,7 @@ class CardServiceTests {
   @Test
   void listAllReturnsZeroWhenNotFindData() {
     when(this.cardRepository.findAll()).thenReturn(List.of());
-    when(this.userRepository.findById(any())).thenReturn(loadUserEntity());
+    when(this.userRepository.findById(any())).thenReturn(CardServiceStub.loadUserEntity());
 
     List<ShowCardResponse> result = cardService.listAllByUserUuid(UUID.randomUUID());
 
@@ -61,41 +57,14 @@ class CardServiceTests {
 
   @Test
   void shouldReturnValueCorrectOnSaveValue() {
-    CardEntity cardEntityFaker = this.loadFakeCardEntity();
+    CardEntity cardEntityFaker = CardServiceStub.loadFakeCardEntity();
     when(this.cardRepository.save(any(CardEntity.class))).thenReturn(cardEntityFaker);
-    when(this.userRepository.findById(any())).thenReturn(loadUserEntity());
+    when(this.userRepository.findById(any())).thenReturn(CardServiceStub.loadUserEntity());
 
-    ShowCardResponse result = this.cardService.addCard(this.loadAddCardDto(),
+    ShowCardResponse result = this.cardService.addCard(CardServiceStub.loadAddCardDto(),
         UUID.fromString(this.faker.internet().uuid()));
 
     assertEquals(cardEntityFaker.getName(), result.getName());
   }
 
-  private AddCardRequest loadAddCardDto() {
-    return AddCardRequest.builder().name(faker.name().fullName()).color(faker.color().hex())
-        .currentLimit(faker.number().randomDouble(10, 0, 100)).flag(faker.random().toString())
-        .dueDate(faker.date().toString()).closeDate(faker.date().toString())
-        .type(CardTypesEnum.CREDIT.toString()).build();
-  }
-
-  private List<CardEntity> loadFakeCardEntities() {
-
-    CardEntity cardEntityOne = this.loadFakeCardEntity();
-    CardEntity cardEntityTwo = this.loadFakeCardEntity();
-
-    return List.of(cardEntityOne, cardEntityTwo);
-  }
-
-  private CardEntity loadFakeCardEntity() {
-
-    return CardEntity.builder().uuid(faker.idNumber().toString()).name(faker.name().fullName())
-        .color(faker.color().hex()).currentLimit(faker.number().randomDouble(10, 0, 100))
-        .flag(faker.random().toString()).dueDate(faker.date().toString())
-        .closeDate(faker.date().toString()).type(CardTypesEnum.CREDIT.toString()).build();
-  }
-
-  private Optional<UserEntity> loadUserEntity() {
-    return Optional.of(UserEntity.builder().username(faker.internet().emailAddress())
-        .password(faker.internet().password()).userId(UUID.randomUUID()).build());
-  }
 }
